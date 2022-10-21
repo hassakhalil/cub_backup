9f0dffce4b3400f7228c4a12e846a03d5a693d7b
hassan khalil <hkhalil@student.1337.ma>
2022-10-21 10:07:24 +0100

    rendering first frame

---
+++ execution/cub3D.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3D.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hkhalil <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/20 02:58:20 by hkhalil           #+#    #+#             */
+/*   Updated: 2022/10/21 00:01:48 by hkhalil          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+#include <stdio.h>
+#include <mlx.h>
+/*
+typedef struct s_data {
+	int	map_rows;
+	int	map_columns;
+	int	cube;
+	int window_length;
+	int window_width;
+	int	**map;
+}				t_data;
+*/
+
+//void	map_2d(/*take struct of data*/);
+#endif
