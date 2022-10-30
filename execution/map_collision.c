39e346d5e265b2187144dc84a8b7b75f94471bc7
hassan khalil <hkhalil@student.1337.ma>
2022-10-30 22:23:03 +0100

    fixing walls collision

--- execution/map_collision.c
+++ execution/map_collision.c
@@ -6,7 +6,7 @@
 /*   By: hkhalil <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 00:38:57 by hkhalil           #+#    #+#             */
-/*   Updated: 2022/10/30 20:45:58 by hkhalil          ###   ########.fr       */
+/*   Updated: 2022/10/30 22:17:48 by hkhalil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,19 +23,26 @@ int	wall(t_data *game, double x, double y, int flag)
 	j = floor(x / game->cube);
 	if (flag)
 	{
-		//debug
-		dprintf(2, "flag == 1\n");
-		//end debug
 		if (M_PI < norm_angle(game->angle) && 1.5*M_PI > norm_angle(game->angle))
 		{
-			//debug
-			dprintf(2, "++++++++++++++++++++++++++++++++++++++++++++\n");
-			//end debug
 			if ((i + 1) <game->map_rows && (j + 1) < game->map_columns && game->map[(int)(i)][(int)(j+1)] == '1' && game->map[(int)(i+1)][(int)(j)] == '1')
 				return(1);
 		}
-		//else if (M_PI_2 < game->angle && M_PI > game->angle)
-		//	if ((i - 1) > 0 && (j - 1) > 0 && game->map[(int)(i-1)][(int)j] == '1' && game->map[(int)i][(int)(j-1)] == '1')
+		else if (1.5*M_PI < norm_angle(game->angle) && 2*M_PI > norm_angle(game->angle))
+		{
+			if ((i + 1) <game->map_rows && (j - 1) < game->map_columns && game->map[(int)(i)][(int)(j-1)] == '1' && game->map[(int)(i+1)][(int)(j)] == '1')
+				return(1);
+		}
+		else if (0 < norm_angle(game->angle) && M_PI_2 > norm_angle(game->angle))
+		{
+			if ((i - 1) <game->map_rows && (j - 1) < game->map_columns && game->map[(int)(i)][(int)(j-1)] == '1' && game->map[(int)(i-1)][(int)(j)] == '1')
+				return(1);
+		}
+		else if (M_PI_2 < norm_angle(game->angle) && M_PI > norm_angle(game->angle))
+		{
+			if ((i - 1) <game->map_rows && (j + 1) < game->map_columns && game->map[(int)(i)][(int)(j+1)] == '1' && game->map[(int)(i-1)][(int)(j)] == '1')
+				return(1);
+		}
 	}
 	return (game->map[(int)i][(int)j] - 48);
 }
