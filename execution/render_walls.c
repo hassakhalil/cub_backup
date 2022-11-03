ad3b197034ecd278e7dcce0419ab91781ffb859c
hassan khalil <hkhalil@student.1337.ma>
2022-11-03 05:37:08 +0100

    fixed fishbowl effect

--- execution/render_walls.c
+++ execution/render_walls.c
@@ -6,7 +6,7 @@
 /*   By: hkhalil <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 03:33:40 by hkhalil           #+#    #+#             */
-/*   Updated: 2022/11/03 04:11:19 by hkhalil          ###   ########.fr       */
+/*   Updated: 2022/11/03 05:36:35 by hkhalil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ void	draw_wall(t_data *game)
 	int	i;
 	double	ray_angle;
 	double	wallheight;
+	double	beta;
 	//distance from player to projection plan
 	double	d2pp;
 	double	rays[RX];
@@ -25,11 +26,12 @@ void	draw_wall(t_data *game)
 	ray_angle = norm_angle(game->angle - M_PI/6);
 	i = 0;
 	d2pp = (CUBE/2)/tan(M_PI/6);
+	beta = norm_angle(ray_angle - game->angle);
 	while (i < RX)
 	{
 		get_inter_point(game, ray, ray_angle);
 		rays[i] = hypot(game->player_x - ray->inter_x, game->player_y - ray->inter_y);
-		wallheight = round((RY*d2pp/rays[i]));
+		wallheight = round(((RY*d2pp)/(rays[i]*cos(beta))));
 			if (wallheight < RY)
 			{
 				if (!ray->hit)
@@ -38,7 +40,7 @@ void	draw_wall(t_data *game)
 					DDA(i, RY/2 - wallheight/2, i, RY/2 + wallheight/2, game, 0x000000);
 			}
 			else
-				DDA(i, 0, i, RY -1, game, 0x808000);
+				DDA(i, 0, i, RY - 1, game, 0x808000);
 			ray_angle += FOV / RX;
 		i++;
 	}
