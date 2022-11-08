26139c18ac4048f75379b6f5e37d3792c8385d5b
hassan khalil <hkhalil@student.1337.ma>
2022-11-08 23:09:53 +0100

    textures

--- execution/render_walls.c
+++ execution/render_walls.c
@@ -6,7 +6,7 @@
 /*   By: hkhalil <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 03:33:40 by hkhalil           #+#    #+#             */
-/*   Updated: 2022/11/08 22:50:35 by hkhalil          ###   ########.fr       */
+/*   Updated: 2022/11/08 23:08:38 by hkhalil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,53 +42,90 @@ void	draw_wall(t_data *game)
 		wallheight = round(((RY*d2pp)/((game->rays[i]).d*cos(beta))));
 		if (wallheight < RY)
 		{
-			if (game->ray.wall == 'N')
-			{
-				start = RY/2 - wallheight/2;
-				end = RY/2 + wallheight/2;
-				// if (game->ray.v_or_h == 0)
-				// 	offset_x = CUBE  - (int)game->rays[i].inter_x % CUBE;
-				// else
-				// 	offset_x = (int)game->rays[i].inter_y % CUBE;
-				if (fabs(game->rays[i].inter_x - CUBE*round(game->rays[i].inter_x/CUBE)) < fabs(game->rays[i].inter_y - CUBE*round(game->rays[i].inter_y/CUBE)))
-					offset_x = game->rays[i].inter_y - CUBE*floor(game->rays[i].inter_y/CUBE);
-				else
-					offset_x =  CUBE*ceil(game->rays[i].inter_x/CUBE) - game->rays[i].inter_x; 
-				y = start;
-				// offset_y = 0;
-				while (y <  end)
-				{
-					offset_y = (y - start)*(64/wallheight);
-					//DDA(i, RY/2 - wallheight/2, i, RY/2 + wallheight/2, game, 0x808000);
-					// texel_color =  game->textures[0].t_addr[(game->textures[0].t_width*offset_y) + offset_x];
-					//debug
-					texel_color = get_pixel(&game->textures[0], offset_x, offset_y);
-					//end devuf
-					my_mlx_pixel_put(game, i, y, texel_color);
-					// game->addr[RX*y + i] = texel_color;
-					y++;
-					// offset_y++;
-				}
-			}
-			else if (game->ray.wall == 'S')
-				DDA(i, RY/2 - wallheight/2, i, RY/2 + wallheight/2, game, 0x3649C2);
-			else if (game->ray.wall == 'E')
-				DDA(i, RY/2 - wallheight/2, i, RY/2 + wallheight/2, game, 0x8ED3F1);
-			else if (game->ray.wall == 'W')
-				DDA(i, RY/2 - wallheight/2, i, RY/2 + wallheight/2, game, 0xF1D38E);
+			start = RY/2 - wallheight/2;
+			end = RY/2 + wallheight/2;
 		}
 		else
 		{
-			if (game->ray.wall == 'N')
-				DDA(i, 0, i, RY - 1, game, 0x808000);
-			else if (game->ray.wall == 'S')
-				DDA(i, 0, i, RY - 1, game, 0x3649C2);
-			else if (game->ray.wall == 'E')
-				DDA(i, 0, i, RY - 1, game, 0x8ED3F1);
-			else if (game->ray.wall == 'W')
-				DDA(i, 0, i, RY - 1, game, 0xF1D38E);
-				//DDA(i, 0, i, RY - 1, game, 0x808000);
+			start = 0;
+			end = RY;
 		}
+			if (game->ray.wall == 'N')
+			{
+				if (game->ray.v_or_h == 0)
+					offset_x = (int)game->rays[i].inter_y % CUBE;
+				else
+					offset_x = (int)game->rays[i].inter_x % CUBE;
+				// if (fabs(game->rays[i].inter_x - CUBE*round(game->rays[i].inter_x/CUBE)) < fabs(game->rays[i].inter_y - CUBE*round(game->rays[i].inter_y/CUBE)))
+				// 	offset_x = game->rays[i].inter_y - CUBE*floor(game->rays[i].inter_y/CUBE);
+				// else
+				// 	offset_x =  CUBE*ceil(game->rays[i].inter_x/CUBE) - game->rays[i].inter_x; 
+				y = start;
+				while (y <  end)
+				{
+					offset_y = (y + (wallheight/2) - (RY/2))*((float)game->textures[3].t_height/wallheight);
+					texel_color = get_pixel(&game->textures[0], offset_x, offset_y);
+					my_mlx_pixel_put(game, i, y, texel_color);
+					y++;
+				}
+			}
+			else if (game->ray.wall == 'S')
+			{
+				if (game->ray.v_or_h == 0)
+					offset_x = (int)game->rays[i].inter_y % CUBE;
+				else
+					offset_x = (int)game->rays[i].inter_x % CUBE;
+				// if (fabs(game->rays[i].inter_x - CUBE*round(game->rays[i].inter_x/CUBE)) < fabs(game->rays[i].inter_y - CUBE*round(game->rays[i].inter_y/CUBE)))
+				// 	offset_x = game->rays[i].inter_y - CUBE*floor(game->rays[i].inter_y/CUBE);
+				// else
+				// 	offset_x =  CUBE*ceil(game->rays[i].inter_x/CUBE) - game->rays[i].inter_x; 
+				y = start;
+				while (y <  end)
+				{
+					offset_y = (y + (wallheight/2) - (RY/2))*((float)game->textures[3].t_height/wallheight);
+					texel_color = get_pixel(&game->textures[1], offset_x, offset_y);
+					my_mlx_pixel_put(game, i, y, texel_color);
+					y++;
+				}
+			}
+			else if (game->ray.wall == 'E')
+			{
+				if (game->ray.v_or_h == 0)
+					offset_x = (int)game->rays[i].inter_y % CUBE;
+				else
+					offset_x = (int)game->rays[i].inter_x % CUBE;
+				// if (fabs(game->rays[i].inter_x - CUBE*round(game->rays[i].inter_x/CUBE)) < fabs(game->rays[i].inter_y - CUBE*round(game->rays[i].inter_y/CUBE)))
+				// 	offset_x = game->rays[i].inter_y - CUBE*floor(game->rays[i].inter_y/CUBE);
+				// else
+				// 	offset_x =  CUBE*ceil(game->rays[i].inter_x/CUBE) - game->rays[i].inter_x; 
+				y = start;
+				while (y <  end)
+				{
+					offset_y = (y + (wallheight/2) - (RY/2))*((float)game->textures[3].t_height/wallheight);
+					texel_color = get_pixel(&game->textures[2], offset_x, offset_y);
+					my_mlx_pixel_put(game, i, y, texel_color);
+					y++;
+				}
+			}
+			else if (game->ray.wall == 'W')
+			{
+				if (game->ray.v_or_h == 0)
+					offset_x = (int)game->rays[i].inter_y % CUBE;
+				else
+					offset_x = (int)game->rays[i].inter_x % CUBE;
+				// if (fabs(game->rays[i].inter_x - CUBE*round(game->rays[i].inter_x/CUBE)) < fabs(game->rays[i].inter_y - CUBE*round(game->rays[i].inter_y/CUBE)))
+				// 	offset_x = game->rays[i].inter_y - CUBE*floor(game->rays[i].inter_y/CUBE);
+				// else
+				// 	offset_x = CUBE*ceil(game->rays[i].inter_x/CUBE) - game->rays[i].inter_x;
+				y = start;
+				while (y <  end)
+				{
+					offset_y = (y + (wallheight/2) - (RY/2))*((float)game->textures[3].t_height/wallheight);
+					texel_color = get_pixel(&game->textures[3], offset_x, offset_y);
+					my_mlx_pixel_put(game, i, y, texel_color);
+					y++;
+				}
+			}
 		ray_angle += FOV / RX;
 		i++;
 	}
