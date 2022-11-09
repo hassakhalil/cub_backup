e2138804f7d35b0a935a0161bacfd230c5093f13
hassan khalil <hkhalil@student.1337.ma>
2022-11-09 02:11:36 +0100

    norm

--- execution/main.c
+++ execution/main.c
@@ -6,39 +6,21 @@
 /*   By: hkhalil <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 01:58:49 by hkhalil           #+#    #+#             */
-/*   Updated: 2022/11/09 02:00:25 by hkhalil          ###   ########.fr       */
+/*   Updated: 2022/11/09 02:09:40 by hkhalil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
 
-//use get_inter one time
-
-int main(int ac, char **av)
+void	game_init(t_data *game, t_info *info)
 {
-	char	**tab;
-	t_info	*info;
-	int		fd;
-
-	fd = 0;
-	(void)ac;
-	tab = init_file(av[1], fd);
-	if (to_parse(tab) != 0)
-	{
-		close(fd);
-		free_tab(tab);
-		return (0);
-	}
-	info = fill_infos(tab);
-	free_tab(tab);
-	t_data *game = malloc(sizeof(t_data));
 	game->map = info->new_map;
 	game->map_rows = info->map_y;
 	game->map_columns = info->map_x;
 	game->map_length = info->map_y * CUBE;
 	game->map_width = info->map_x * CUBE;
-	game->player_x = info->x*CUBE + CUBE/2;
-	game->player_y = info->y*CUBE + CUBE/2;
+	game->player_x = info->x * CUBE + CUBE / 2;
+	game->player_y = info->y * CUBE + CUBE / 2;
 	game->floor = info->floor;
 	game->ceilling = info->ceil;
 	game->angle = ft_angle(info->dir);
@@ -53,6 +35,28 @@ int main(int ac, char **av)
 	render_walls(game);
 	render_map(game);
 	mlx_put_image_to_window(game->mlx, game->mlx_window, game->img, 0, 0);
+}
+
+int	main(int ac, char **av)
+{
+	char	**tab;
+	t_info	*info;
+	t_data	*game;
+	int		fd;
+
+	fd = 0;
+	(void)ac;
+	tab = init_file(av[1], fd);
+	if (to_parse(tab) != 0)
+	{
+		close(fd);
+		free_tab(tab);
+		return (0);
+	}
+	info = fill_infos(tab);
+	free_tab(tab);
+	game = malloc(sizeof(t_data));
+	game_init(game, info);
 	mlx_hook(game->mlx_window, 17, 0, ft_exit, game);
 	mlx_hook(game->mlx_window, 2, 0, key_hook, game);
 	mlx_mouse_hook(game->mlx_window, mouse_hook, game);
