bd8f21616caff0122b3e738a8142f4633b327065
hassan khalil <hkhalil@student.1337.ma>
2022-11-09 02:16:50 +0100

    norm

--- execution/floor_ceilling.c
+++ execution/floor_ceilling.c
@@ -6,7 +6,7 @@
 /*   By: hkhalil <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/05 22:58:09 by hkhalil           #+#    #+#             */
-/*   Updated: 2022/11/09 01:31:03 by hkhalil          ###   ########.fr       */
+/*   Updated: 2022/11/09 02:15:53 by hkhalil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,25 +19,19 @@ void	floor_ceilling(t_data *game)
 	int	end_ceilling;
 
 	i = 0;
-	end_ceilling = RY/2;
+	end_ceilling = RY / 2;
 	while (i < end_ceilling)
 	{
 		j = 0;
 		while (j < RX)
-		{
-			my_mlx_pixel_put(game, j, i, game->ceilling);
-			j++;
-		}
+			my_mlx_pixel_put(game, j++, i, game->ceilling);
 		i++;
 	}
 	while (i < RY)
 	{
 		j = 0;
 		while (j < RX)
-		{
-			my_mlx_pixel_put(game, j, i, game->floor);
-			j++;
-		}
+			my_mlx_pixel_put(game, j++, i, game->floor);
 		i++;
 	}
 }
