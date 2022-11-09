bcc8f50999dc6d67b8e46703b17cb8e8cd0c6907
hassan khalil <hkhalil@student.1337.ma>
2022-11-09 02:40:19 +0100

    norm

--- execution/render_map.c
+++ execution/render_map.c
@@ -6,7 +6,7 @@
 /*   By: hkhalil <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 00:45:16 by hkhalil           #+#    #+#             */
-/*   Updated: 2022/11/09 01:22:31 by hkhalil          ###   ########.fr       */
+/*   Updated: 2022/11/09 02:38:08 by hkhalil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	char	*dst;
 
 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-	*(unsigned int*)dst = color;
+	*(unsigned int *)dst = color;
 }
 
 unsigned int	get_pixel(t_texture *data, int x, int y)
@@ -25,39 +25,47 @@ unsigned int	get_pixel(t_texture *data, int x, int y)
 	char			*dst;
 
 	dst = data->t_addr + (y * data->ll + x * (data->bpp / 8));
-	return (*(unsigned int*)dst);
+	return (*(unsigned int *)dst);
+}
+
+void	draw_map(t_data *game, int i, int j)
+{
+	int	k;
+	int	l;
+
+	k = 0;
+	while (k < CUBE)
+	{
+		l = 0;
+		while (l < CUBE)
+		{
+			if ((game->map)[i][j] == '1')
+				my_mlx_pixel_put(game, MSF * (CUBE * j + l),
+					MSF * (CUBE * i + k), 0x000000);
+			if (pow(((CUBE * i + k) - (game->player_y)), 2)
+				+ pow(CUBE * j + l - (game->player_x), 2) <= 200)
+				my_mlx_pixel_put(game, MSF * (CUBE * j + l),
+					MSF * (CUBE * i + k), 0xFF0000);
+			l++;
+		}
+		k++;
+	}
 }
 
 void	render_map(t_data *game)
 {
-	int i;
-	int j;
-	int k;
-	int l;
-	
+	int	i;
+	int	j;
 
 	i = 0;
 	while (i < game->map_rows)
 	{
- 			j = 0;
-			while (j < game->map_columns)
-			{
-				k = 0;
-				while (k < CUBE)
-				{
-					l = 0;
-					while (l < CUBE)
-					{
-						if ((game->map)[i][j] == '1')
-							my_mlx_pixel_put(game, MSF*(CUBE*j+l), MSF*(CUBE*i+k), 0x000000);
-						if (pow(((CUBE*i + k)-(game->player_y)), 2) + pow(CUBE*j + l-(game->player_x), 2) <= 200)
-							my_mlx_pixel_put(game, MSF*(CUBE*j+l), MSF*(CUBE*i+k), 0xFF0000);
-						l++;
-					}
-					k++;
-				}
-				j++;
-			}
-			i++;
+		j = 0;
+		while (j < game->map_columns)
+		{
+			draw_map(game, i, j);
+			j++;
+		}
+		i++;
 	}
 }
