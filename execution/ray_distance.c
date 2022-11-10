6ec4632aa874d921f003b157bf231024ecdb6e6c
hassan khalil <hkhalil@student.1337.ma>
2022-11-10 08:09:58 +0100

    norm done

--- execution/ray_distance.c
+++ execution/ray_distance.c
@@ -6,7 +6,7 @@
 /*   By: hkhalil <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 06:35:08 by hkhalil           #+#    #+#             */
-/*   Updated: 2022/11/10 07:55:13 by hkhalil          ###   ########.fr       */
+/*   Updated: 2022/11/10 08:08:02 by hkhalil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,8 @@ void	first_inter_hor(t_data *game, double ray_ang, double *correcth)
 		game->ray.y_hor += CUBE;
 	else
 		*correcth = -1;
-	game->ray.x_hor = game->player_x + (game->ray.y_hor - game->player_y) / tan(ray_ang);
+	game->ray.x_hor = game->player_x
+		+ (game->ray.y_hor - game->player_y) / tan(ray_ang);
 }
 
 void	delta_hor(double ray_ang, double *delta_yh, double *delta_xh)
@@ -29,7 +30,9 @@ void	delta_hor(double ray_ang, double *delta_yh, double *delta_xh)
 	if (!(ray_ang > 0 && ray_ang < M_PI))
 		*delta_yh *= -1;
 	*delta_xh = CUBE / tan(ray_ang);
-	if ((!(ray_ang < M_PI_2  || ray_ang > 1.5*M_PI) && *delta_xh > 0) || ((ray_ang < M_PI_2  || ray_ang > 1.5*M_PI) && *delta_xh < 0))
+	if ((!(ray_ang < M_PI_2 || ray_ang > 1.5 * M_PI)
+			&& *delta_xh > 0) || ((ray_ang < M_PI_2
+				|| ray_ang > 1.5 * M_PI) && *delta_xh < 0))
 		*delta_xh *= -1;
 }
 
@@ -40,15 +43,15 @@ void	get_hor(t_data *game, double ray_ang)
 	double	delta_yh;
 
 	game->ray.flag_hor = 0;
-	///hor_inter code
 	first_inter_hor(game, ray_ang, &correcth);
 	delta_hor(ray_ang, &delta_yh, &delta_xh);
 	while (1)
 	{
-		if (game->ray.x_hor > 0 && game->ray.y_hor > 0 && game->ray.x_hor < game->map_width && game->ray.y_hor < game->map_length)
+		if (game->ray.x_hor > 0 && game->ray.y_hor > 0 && game->ray.x_hor
+			< game->map_width && game->ray.y_hor < game->map_length)
 		{
 			if (wall(game, game->ray.x_hor, game->ray.y_hor + correcth, 0) == 1)
-				break;
+				break ;
 			game->ray.x_hor += delta_xh;
 			game->ray.y_hor += delta_yh;
 		}
@@ -60,6 +63,29 @@ void	get_hor(t_data *game, double ray_ang)
 	}
 }
 
+void	first_inter_ver(t_data *game, double ray_ang, double *correctv)
+{
+	*correctv = 0;
+	game->ray.x_ver = floor(game->player_x / CUBE) * CUBE;
+	if (ray_ang < M_PI_2 || ray_ang > 1.5 * M_PI)
+		game->ray.x_ver += CUBE;
+	else
+		*correctv = -1;
+	game->ray.y_ver = game->player_y
+		+ (game->ray.x_ver - game->player_x) * tan(ray_ang);
+}
+
+void	delta_ver(double ray_ang, double *delta_yv, double *delta_xv)
+{
+	*delta_xv = CUBE;
+	if (!(ray_ang < M_PI_2 || ray_ang > 1.5 * M_PI))
+		*delta_xv *= -1;
+	*delta_yv = CUBE * tan(ray_ang);
+	if ((!(ray_ang > 0 && ray_ang < M_PI) && *delta_yv > 0)
+		|| ((ray_ang > 0 && ray_ang < M_PI) && *delta_yv < 0))
+		*delta_yv *= -1;
+}
+
 void	get_ver(t_data *game, double ray_ang)
 {
 	double	correctv;
@@ -67,28 +93,15 @@ void	get_ver(t_data *game, double ray_ang)
 	double	delta_yv;
 
 	game->ray.flag_ver = 0;
-	//ver_inter
-	//first intersection
-	correctv = 0;
-	game->ray.x_ver = floor(game->player_x / CUBE) * CUBE;
-	if (ray_ang < M_PI_2  || ray_ang > 1.5*M_PI)
-		game->ray.x_ver += CUBE;
-	else
-		correctv = -1;
-	game->ray.y_ver = game->player_y + (game->ray.x_ver - game->player_x)*tan(ray_ang);
-	//finding delta y and delta x
-	delta_xv = CUBE;
-	if (!(ray_ang < M_PI_2  || ray_ang > 1.5*M_PI))
-		delta_xv *= -1;
-	delta_yv = CUBE * tan(ray_ang);
-	if ((!(ray_ang > 0 && ray_ang < M_PI) && delta_yv > 0) || ((ray_ang > 0 && ray_ang < M_PI) && delta_yv < 0))
-		delta_yv *= -1;
-		while (1)
+	first_inter_ver(game, ray_ang, &correctv);
+	delta_ver(ray_ang, &delta_yv, &delta_xv);
+	while (1)
 	{
-		if (game->ray.x_ver > 0 && game->ray.y_ver > 0 && game->ray.x_ver < game->map_width && game->ray.y_ver < game->map_length)
+		if (game->ray.x_ver > 0 && game->ray.y_ver > 0 && game->ray.x_ver
+			< game->map_width && game->ray.y_ver < game->map_length)
 		{
 			if (wall(game, game->ray.x_ver + correctv, game->ray.y_ver, 0) == 1)
-				break;
+				break ;
 			game->ray.x_ver += delta_xv;
 			game->ray.y_ver += delta_yv;
 		}
@@ -121,8 +134,10 @@ void	compare_d(t_data *game)
 	double	d_ver;
 	double	d_hor;
 
-	d_ver = hypot(game->player_x - game->ray.x_ver, game->player_y - game->ray.y_ver);
-	d_hor = hypot(game->player_x - game->ray.x_hor, game->player_y - game->ray.y_hor);
+	d_ver = hypot(game->player_x - game->ray.x_ver,
+			game->player_y - game->ray.y_ver);
+	d_hor = hypot(game->player_x - game->ray.x_hor,
+			game->player_y - game->ray.y_hor);
 	if (d_hor > d_ver)
 	{
 		game->ray.inter_x = game->ray.x_ver;
@@ -141,7 +156,7 @@ void	get_inter_point(t_data *game, double ang)
 {
 	double	ray_ang;
 
-	ray_ang =  norm_angle(ang);
+	ray_ang = norm_angle(ang);
 	get_hor(game, ray_ang);
 	get_ver(game, ray_ang);
 	if (game->ray.flag_ver || game->ray.flag_hor)
@@ -152,8 +167,8 @@ void	get_inter_point(t_data *game, double ang)
 		game->ray.wall = 'S';
 	else if (game->ray.v_or_h == 'h')
 		game->ray.wall = 'N';
-	 else if (ray_ang < M_PI_2 || ray_ang > 1.5*M_PI)
-	 	game->ray.wall = 'E';
+	else if (ray_ang < M_PI_2 || ray_ang > 1.5 * M_PI)
+		game->ray.wall = 'E';
 	else
-	 	game->ray.wall = 'W';
+		game->ray.wall = 'W';
 }
