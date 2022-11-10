c58f095fc21934b4b27dec65a816dc042ad8e17c
hassan khalil <hkhalil@student.1337.ma>
2022-11-10 07:56:22 +0100

    norm

--- execution/ray_distance.c
+++ execution/ray_distance.c
@@ -6,11 +6,32 @@
 /*   By: hkhalil <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 06:35:08 by hkhalil           #+#    #+#             */
-/*   Updated: 2022/11/10 07:44:44 by hkhalil          ###   ########.fr       */
+/*   Updated: 2022/11/10 07:55:13 by hkhalil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
+
+void	first_inter_hor(t_data *game, double ray_ang, double *correcth)
+{
+	*correcth = 0;
+	game->ray.y_hor = floor(game->player_y / CUBE) * CUBE;
+	if (ray_ang > 0 && ray_ang < M_PI)
+		game->ray.y_hor += CUBE;
+	else
+		*correcth = -1;
+	game->ray.x_hor = game->player_x + (game->ray.y_hor - game->player_y) / tan(ray_ang);
+}
+
+void	delta_hor(double ray_ang, double *delta_yh, double *delta_xh)
+{
+	*delta_yh = CUBE;
+	if (!(ray_ang > 0 && ray_ang < M_PI))
+		*delta_yh *= -1;
+	*delta_xh = CUBE / tan(ray_ang);
+	if ((!(ray_ang < M_PI_2  || ray_ang > 1.5*M_PI) && *delta_xh > 0) || ((ray_ang < M_PI_2  || ray_ang > 1.5*M_PI) && *delta_xh < 0))
+		*delta_xh *= -1;
+}
 
 void	get_hor(t_data *game, double ray_ang)
 {
@@ -20,21 +41,8 @@ void	get_hor(t_data *game, double ray_ang)
 
 	game->ray.flag_hor = 0;
 	///hor_inter code
-	//first intersection
-	correcth = 0;
-	game->ray.y_hor = floor(game->player_y / CUBE) * CUBE;
-	if (ray_ang > 0 && ray_ang < M_PI)
-		game->ray.y_hor += CUBE;
-	else
-		correcth = -1;
-	game->ray.x_hor = game->player_x + (game->ray.y_hor - game->player_y) / tan(ray_ang);
-	//finding delta x and delta y
-	delta_yh = CUBE;
-	if (!(ray_ang > 0 && ray_ang < M_PI))
-		delta_yh *= -1;
-	delta_xh = CUBE / tan(ray_ang);
-	if ((!(ray_ang < M_PI_2  || ray_ang > 1.5*M_PI) && delta_xh > 0) || ((ray_ang < M_PI_2  || ray_ang > 1.5*M_PI) && delta_xh < 0))
-		delta_xh *= -1;
+	first_inter_hor(game, ray_ang, &correcth);
+	delta_hor(ray_ang, &delta_yh, &delta_xh);
 	while (1)
 	{
 		if (game->ray.x_hor > 0 && game->ray.y_hor > 0 && game->ray.x_hor < game->map_width && game->ray.y_hor < game->map_length)
